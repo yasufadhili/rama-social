d591c8d5350e539360959738858a8ba369cdbc7f
Yasu Fadhili <yasufadhili@gmail.com>
2024-08-05 15:05:26 +0300

    .

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -5,14 +5,9 @@ import { useAuth } from '@/context/AuthProvider';
 export default function AppLayout() {
   const { user, intialising } = useAuth();
 
-  // You can keep the splash screen open, or render a loading screen like we do here.
-  if (intialising) {
-    return <Text>Loading...</Text>;
-  }
-
   // Only require authentication within the (app) group's layout as users
   // need to be able to access the (auth) group and sign in again.
-  if (user === null) {
+  if (!user) {
     // On web, static rendering will stop here as the user is not authenticated
     // in the headless Node process that the pages are rendered in.
     return <Redirect href="/login" />;
