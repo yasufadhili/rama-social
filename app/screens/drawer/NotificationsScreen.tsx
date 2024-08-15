af2b66d9a1b2270e64e84d8d028ab80f351ba5b6
Yasu Fadhili <yasufadhili@gmail.com>
2024-08-15 23:07:26 +0300

    Setup natifications layout

--- app/screens/drawer/NotificationsScreen.tsx
+++ app/screens/drawer/NotificationsScreen.tsx
@@ -1,3 +1,43 @@
+import { RamaBackView } from '@/components/Themed';
+import { useTheme } from '@/context/ThemeContext';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+const Tab = createMaterialTopTabNavigator();
+
 export default function NotificationsScreen(){
-    return <></>
+    return <TopTabs />
+}
+
+function TopTabs(){
+    const {colours} = useTheme();
+    return <Tab.Navigator screenOptions={{
+        tabBarStyle: {
+            backgroundColor: colours.background.strong
+        },
+        tabBarLabelStyle: {
+            textTransform: "capitalize",
+            fontSize: 14
+        },
+        tabBarIndicatorStyle: {
+            backgroundColor: colours.primary,
+            width: "20%",
+            marginLeft: "15%"
+        },
+        tabBarIndicatorContainerStyle: {
+            alignSelf: "center",
+            justifyContent: "center",
+            alignItems: "center"
+        }
+    }}>
+        <Tab.Screen name={"AllNotificationsTab"} component={AllNotificationsTab} options={{title: "All"}} />
+        <Tab.Screen name={"MentionsNotificationsTab"} component={MentionsNotificationsTab} options={{title: "Mentions"}} />
+    </Tab.Navigator>
+}
+
+function AllNotificationsTab(){
+    return <RamaBackView />
+}
+
+function MentionsNotificationsTab(){
+    return <RamaBackView />
 }
