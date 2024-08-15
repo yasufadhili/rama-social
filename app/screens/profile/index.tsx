e3e020c69a3f900f9fa871a1504b2a0c7b504eae
Yasu Fadhili <yasufadhili@gmail.com>
2024-08-15 23:29:34 +0300

    Added edit profile route

--- app/screens/profile/index.tsx
+++ app/screens/profile/index.tsx
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import ProfileDetailsScreen from "./ProfileDetailsScreen";
 import SetupProfileScreen from "./SetupProfileScreen";
+import EditProfileScreen from "./EditProfileScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -8,5 +9,6 @@ export default function ProfileStack(){
     return <Stack.Navigator>
         <Stack.Screen name={"ProfileDetailsScreen"} component={ProfileDetailsScreen} />
         <Stack.Screen name={"SetupProfileScreen"} component={SetupProfileScreen} />
+        <Stack.Screen name={"EditProfileScreen"} component={EditProfileScreen} />
     </Stack.Navigator>
 }
