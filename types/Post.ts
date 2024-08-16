62f1906836ccfe07d82ba4a606d9bf5578046998
Yasu Fadhili <yasufadhili@gmail.com>
2024-08-16 03:41:43 +0300

    Update post types

--- types/Post.ts
+++ types/Post.ts
@@ -1,5 +1,20 @@
 import { FirebaseFirestoreTypes } from "@react-native-firebase/firestore";
 
+export type TPost = {
+  id?: string;
+  creatorPhotoUrl: string;
+  creatorDisplayName: string;
+  caption?: string;
+  mediaUrls?: string[];
+  images?: string[];
+  videos?: string[];
+  isPublic?: boolean;
+  creatorId?: string;
+  createdAt: FirebaseFirestoreTypes.Timestamp;
+  post_type?: "media" | "text" | "audio";
+  textBlocks: TTextBlock[];
+  gradientColours: string[];
+}
 
 export type TMediaPost = {
     id?: string;
@@ -11,4 +26,27 @@ export type TMediaPost = {
     creatorId?: string;
     createdAt?: FirebaseFirestoreTypes.Timestamp;
     post_type?: "media";
-  }
+}
+
+export type TTextPost = {
+  id: string,
+  creatorPhotoUrl: string;
+  creatorDisplayName: string;
+  creatorId: string;
+  post_type: "text";
+  textBlocks: TTextBlock[];
+  gradientColours: string[];
+  isPublic?: boolean;
+  createdAt: FirebaseFirestoreTypes.Timestamp;
+};
+
+export type TTextBlock = {
+  id: string;
+  text: string;
+  style: {
+    fontWeight: 'normal' | 'bold';
+    fontStyle: 'normal' | 'italic';
+    textDecorationLine: 'none' | 'underline';
+    fontSize: number;
+  };
+};
