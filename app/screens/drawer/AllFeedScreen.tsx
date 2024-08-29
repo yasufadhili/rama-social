49d0e7429bc43ca45b4e6da72cf17bf82653d118
Yasu Fadhili <yasufadhili@gmail.com>
2024-08-29 18:13:33 +0300

    Update feed

--- app/screens/drawer/AllFeedScreen.tsx
+++ app/screens/drawer/AllFeedScreen.tsx
@@ -18,7 +18,6 @@ export default function AllFeedScreen(){
     return <>
     <RamaBackView>
         
-        <FeedScreen />
         <LeftMessagesFAB />
         <RightCreateFAB />
     </RamaBackView>
